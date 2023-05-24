0194746b6503cd12e770177deda16944bbe83972
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-05-24 11:51:27 +0200

    header and footer php

---
+++ index.php
@@ -0,0 +1,188 @@
+<?php require "includes/header.php"; ?>
+<?php require "config/config.php"; ?>
+
+<!--New products-->
+
+<div class="categories">
+  <div class="small-container">
+    <div class="row">
+      <div class="col-3">
+        <img src="https://i.ibb.co/sqnY1pG/category-1.jpg" alt="" />
+      </div>
+      <div class="col-3">
+        <img src="https://i.ibb.co/GCJLQRQ/category-2.jpg" alt="" />
+      </div>
+      <div class="col-3">
+        <img src="https://i.ibb.co/wYsXqP5/category-3.jpg" alt="" />
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="small-container">
+  <h2 class="title">Featured Products</h2>
+  <div class="row">
+    <div class="col-4">
+      <a href="product_details.html"><img src="https://i.ibb.co/47Sk9QL/product-1.jpg" alt="" /></a>
+      <a href="product_details.html">
+        <h4>Red Printed T-shirt</h4>
+      </a>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/b7ZVzYr/product-2.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/KsMVr26/product-3.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/0cMfpcr/product-4.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+  </div>
+  <h2 class="title">Latest Products</h2>
+  <div class="row">
+    <div class="col-4">
+      <img src="https://i.ibb.co/bQ5t8bR/product-5.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/vVpTyBD/product-6.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/hR5FGwH/product-7.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/QfCgdXZ/product-8.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-4">
+      <img src="https://i.ibb.co/nw5xZwk/product-9.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/9HCsmjf/product-10.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/JQ2MBHR/product-11.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+
+    <div class="col-4">
+      <img src="https://i.ibb.co/nRZMs6Y/product-12.jpg" alt="" />
+      <h4>Red Printed T-shirt</h4>
+      <div class="rating">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <p>R500.00</p>
+    </div>
+  </div>
+</div>
+<?php require "includes/footer.php"; ?>

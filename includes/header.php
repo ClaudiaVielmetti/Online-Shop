5c360bd291886edbb3ef386c3e316dc9aedefd18
ClaudiaV27 <claudiavielmetti38@gmail.com>
2023-05-29 11:46:16 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -53,8 +53,8 @@ define("APPURL", "http://localhost/online-shop/");
                     <li><a href="./lookbook.html">LOOKBOOK</a></li>
                     <li><a href="./shop.html">SHOP</a></li>
                     <li><a href="./contact.html">CONTACT</a></li>
-                    <li><a href="./contact.html">LOGIN</a></li>
-                    <li><a href="./contact.html">REGISTER</a></li>
+                    <li><a href="./auth/login.php">LOGIN</a></li>
+                    <li><a href="./auth/register.php">REGISTER</a></li>
                   
                     <li>
                         <a href="./cart.html"><i class="bi bi-bag" id="cart-icon"></i></a>
